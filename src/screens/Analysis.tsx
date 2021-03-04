70feedeb002febc328ed0f0e1cce5469050552de
Eimantas <eimantas@riawolf.com>
2021-03-04 15:55:50 +0200

    - set up typescript; - set up routes; - added header;

---
+++ src/screens/Analysis.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Analysis = () => {
+    return (
+        <div className="ScreenWrapper">
+            <h2>Analysis!</h2>
+        </div>
+    )
+};
+
+export default Analysis;
